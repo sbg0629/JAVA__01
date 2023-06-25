a4bb84be669dbf425a70c17cac579cb6ecbbb4d9
손봉균 <124877603+sbg0629@users.noreply.github.com>
2023-06-26 03:49:42 +0900

    문자열

---
+++ src/Chap_01/_02_DataType.java
@@ -0,0 +1,15 @@
+package chap01;
+
+public class _02_DataType {
+    public static void main(String[] args) {
+        System.out.println("hello"); //문자열
+        System.out.println("안녕하세요1");
+        System.out.println(12); //12출력 숫자 자료형
+        System.out.println(-34); //-34출력 숫자 자료형
+        System.out.println(3.14); //3.14 출력 숫자 자료형
+        System.out.println(true);//분리 자료형
+        System.out.println(false);//분리 자료형
+
+        System.out.println(123 + 345); //더하기 연산
+    }
+}
