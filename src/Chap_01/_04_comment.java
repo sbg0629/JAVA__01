f143218627ce9af5f4b709e5fa4c2626f3078a66
손봉균 <124877603+sbg0629@users.noreply.github.com>
2023-06-26 03:50:02 +0900

    주석 달기 여러개 주석 = 컨트롤 + 쉬프트 + /

---
+++ src/Chap_01/_04_comment.java
@@ -0,0 +1,17 @@
+package chap01;
+
+public class _04_comment {
+    public static void main(String[] args) {
+        System.out.println("(10분전) 잠시 후 결혼식 시작 예정이오니 착석 부탁드립니다.");
+        //System.out.println("(5분전) 잠시 후 결혼식 시작 예정이오니 착석 부탁드립니다."); 주석
+        System.out.println("지금부터 결혼식을 시작하겠습니다.");
+
+        int size = 120;
+        size = size + 10; //130
+        System.out.println("신발 사이즈 " + size + "으로 보여주세요 ");
+
+       /* int a = 10;
+        int b = 20;
+        System.out.println(a+b); // 30*/ // /**/ 여러개 주석 = 컨트롤 + 쉬프트 + /
+    }
+}
