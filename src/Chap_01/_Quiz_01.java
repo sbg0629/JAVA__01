b5384f96299955f4fb60485ad8df0a69ff42c3db
손봉균 <124877603+sbg0629@users.noreply.github.com>
2023-06-26 03:50:47 +0900

    퀴즈1. 버스 이름 버스 번호 남은 시간 남은 거리 출력하기.

---
+++ src/Chap_01/_Quiz_01.java
@@ -0,0 +1,15 @@
+package chap01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        String Bus = "상암08"; //버스 이름
+        int Bus_num = 1234; //버스 번호
+        int min = 3; //남은 시간
+        double distance = 1.5; // 남은 거리
+        System.out.println("버스 번호는 "+ Bus_num + Bus +" 버스");
+        System.out.println(min + "분 남았습니다.");
+        System.out.println("남은 거리는 " + distance + "km 입니다." );
+
+
+    }
+}
