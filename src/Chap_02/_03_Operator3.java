bbd464917ef4b1d786ccc3c317bd86cc83352f53
손봉균 <124877603+sbg0629@users.noreply.github.com>
2023-06-26 12:34:30 +0900

    비교 연산자.

---
+++ src/Chap_02/_03_Operator3.java
@@ -0,0 +1,17 @@
+package Chap_02;
+
+public class _03_Operator3 {
+    public static void main(String[] args) {
+        //비교연산자
+        System.out.println(5 > 3); //참이면 True 거짓이면false
+        System.out.println(5 >= 3);
+        System.out.println(5 >= 5);
+        System.out.println(5 >= 7);
+
+        System.out.println(5 < 3);
+        System.out.println(5 <= 3);
+
+        System.out.println(5 == 5);
+        System.out.println(5 == 3);
+    }
+}
