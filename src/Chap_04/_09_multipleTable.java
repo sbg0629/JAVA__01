f12f2a6a1877aa777851baa1c962f8c8180e329d
손봉균 <124877603+sbg0629@users.noreply.github.com>
2023-07-12 15:21:51 +0900

    구구단 1 * 1 = 1 --- -- ----- --- --- --- 9 * 9 = 81

---
+++ src/Chap_04/_09_multipleTable.java
@@ -0,0 +1,14 @@
+package Chap_04;
+
+public class _09_multipleTable {
+    public static void main(String[] args) {
+        //구구단
+
+        for(int i = 1; i < 10; i++){
+            for(int j = 1; j < 10; j++){
+                System.out.println(i + " * " + j + " = " + i*j);
+            }
+            System.out.println();
+        }
+    }
+}
