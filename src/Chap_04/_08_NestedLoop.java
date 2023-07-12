8cf3c7359bbd23bfee560e03ec545967547247c7
손봉균 <124877603+sbg0629@users.noreply.github.com>
2023-07-12 15:21:28 +0900

    이중 반복문

---
+++ src/Chap_04/_08_NestedLoop.java
@@ -0,0 +1,43 @@
+package Chap_04;
+
+public class _08_NestedLoop {
+    public static void main(String[] args) {
+        //이중 반복문
+
+        //별 (*) 사각형 만들기
+
+        /*
+
+        *****
+        *****
+        *****
+        *****
+         */
+
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j < 5; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //별 왼쪽 삼각형 만들기
+        for(int i = 0; i<5;i++){
+            for (int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        //별 오른쪽 삼각형
+        for(int i = 0; i<5;i++){
+            for (int j = 0; j < 4 -i; j++){
+                System.out.print("S");
+            }
+            for (int k = 0; k <= i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
