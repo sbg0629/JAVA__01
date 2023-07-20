e8123958165f745e4e32b267ab252cfe21da903f
손봉균 <124877603+sbg0629@users.noreply.github.com>
2023-07-20 19:47:37 +0900

    code up 1010

---
+++ src/My_free/code_up.java
@@ -0,0 +1,12 @@
+package My_free;
+
+import java.util.Scanner;
+
+public class code_up {
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+        int x;
+        x = n.nextInt();
+        System.out.println(x);
+    }
+}
